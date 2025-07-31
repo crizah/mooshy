728a73dfea5d63056dc4cf09b75afa05ece0f51e
crizah <shaizah.labique@gmail.com>
2025-07-31 18:31:21 +0530

    lexer identifies keywords and INT Type now

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,6 +1,9 @@
 package lexer
 
-import "mooshy/token"
+import (
+	"fmt"
+	"mooshy/token"
+)
 
 type Lexer struct {
 	input        string
@@ -12,11 +15,16 @@ type Lexer struct {
 
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
+	// l.currPosition = 0 // starting poiunts
+	// l.prevPosition = -1
+	// l.ch = input[l.currPosition]
 	l.readChar()
 	return l
 }
 
 func (l *Lexer) readChar() { // defined like this means we can call it like l.readChar() and it will work on l's data
+	// this is to update pointers of the lexer
+
 	if l.currPosition >= len(l.input) {
 		l.ch = 0 // end of input
 	} else {
@@ -29,6 +37,16 @@ func (l *Lexer) readChar() { // defined like this means we can call it like l.re
 func newToken(operator token.TokenType, character byte) token.Token {
 	tok := token.Token{Type: operator, Literal: string(character)}
 	return tok
+}
+
+var keywords = map[string]token.TokenType{
+	"let":    token.LET,
+	"func":   token.FUNC,
+	"if":     token.IF,
+	"else":   token.ELSE,
+	"return": token.RETURN,
+	"true":   token.TRUE,
+	"false":  token.FALSE,
 }
 
 func (l *Lexer) NextToken() token.Token {
@@ -50,12 +68,106 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.ASSIGN, l.ch)
 	case '+':
 		tok = newToken(token.PLUS, l.ch)
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
+	case '*':
+		tok = newToken(token.MULTIPLY, l.ch)
+	case '/':
+		tok = newToken(token.DIVIDE, l.ch)
+	case '!':
+		tok = newToken(token.NOT, l.ch)
+	case '<':
+		tok = newToken(token.LESSER, l.ch)
+	case '>':
+		tok = newToken(token.GREATER, l.ch)
+
 	case 0:
 		// tok = newToken(token.EOF, "") cant do this cuz newToken changes the byte to string
+		//implement that zero doesnt always have to mean end of file it can be read as 019 which is just int
 		tok.Literal = ""
 		tok.Type = token.EOF
+
+	default:
+		if isLetter(l.ch) { // can be keyword or identifier
+
+			tok.Literal = l.readIdentifier() // function reads the string until non-letter nd then returns its literal
+			tok.Type = l.getType(tok.Literal)
+			// l.readChar()
+			return tok
+
+		}
+		if isWhitespace(l.ch) {
+			for isWhitespace(l.ch) {
+				l.readChar()
+			}
+			return l.NextToken()
+		}
+		if isNumber(l.ch) {
+
+			fmt.Println("yeah")
+			tok.Literal = l.readNumber() // function reads the string until non-number and then returns its literal
+			tok.Type = token.INT
+			// l.readChar()
+
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch) // if its not a letter, then its an illegal token
+		}
+
 	}
 
 	l.readChar() // when we call nextToken again, the pointers are updated
 	return tok
+}
+
+func (l *Lexer) readIdentifier() string {
+	// read until its not a letter and return its literal
+	s := ""
+
+	for isLetter(l.ch) {
+		s += string(l.ch) // append the character to the string
+		l.readChar()      // move to the next character
+
+	}
+
+	return s
+
+}
+
+func isNumber(ch byte) bool {
+	return ch >= '0' && ch <= '9'
+
+}
+
+func (l *Lexer) readNumber() string {
+	// read until no more number and return it as string literal
+	s := ""
+	for isNumber(l.ch) {
+		s += string(l.ch)
+		l.readChar()
+
+	}
+
+	return s
+}
+
+func (l *Lexer) getType(literal string) token.TokenType {
+
+	if tok, ok := keywords[literal]; ok { // map lookup with boolean check
+		return tok // if its a keyword, return the keyword as a token type
+	}
+
+	// if _, err := strconv.Atoi(literal); err == nil { // if its an integer, return the int token type
+	// 	return token.INT
+	// }
+
+	return token.VARIABLE // otherwise, its an identifier or variable
+
+}
+func isLetter(ch byte) bool {
+	return ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z') || (ch == '_')
+}
+
+func isWhitespace(ch byte) bool {
+	return ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r'
 }
--- token/token.go
+++ token/token.go
@@ -1,6 +1,6 @@
 package token
 
-type TokenType string // only capital letter identifiers are exported
+type TokenType string // only capital letter identifiers are exported to other files
 
 type Token struct {
 	Type    TokenType
@@ -9,12 +9,23 @@ type Token struct {
 
 const (
 	// keywords
-	LET  = "LET"
-	FUNC = "FUNC"
+	LET    = "LET"
+	FUNC   = "FUNC"
+	RETURN = "RETURN"
+	IF     = "IF"
+	ELSE   = "ELSE"
+	TRUE   = "TRUE"
+	FALSE  = "FALSE"
 
 	// operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	MULTIPLY = "*"
+	DIVIDE   = "/"
+	NOT      = "!"
+	LESSER   = "<"
+	GREATER  = ">"
 
 	// symbols
 	LPAREN    = "("
@@ -27,9 +38,20 @@ const (
 	// literals and identifiers
 	INT      = "INT"
 	VARIABLE = "VARIABLE"
+	IDENT    = "IDENT" // used for identifiers that are not keywords
 
 	// errors
 	ILLEGAL = "ILLEGAL" // token we have not defined
 	EOF     = "EOF"     // signifies end of file
 
 )
+
+// var keywords = map[string]TokenType{
+// 	"let":    LET,
+// 	"func":   FUNC,
+// 	"if":     IF,
+// 	"else":   ELSE,
+// 	"return": RETURN,
+// 	"true":   TRUE,
+// 	"false":  FALSE,
+// }
